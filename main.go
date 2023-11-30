9e627004b3d03b7a239b180ab2303caceca9c40e
kawashima <kawashima@cloudpack.jp>
2023-11-30 16:52:26 +0900

    chore: add version.go

--- main.go
+++ main.go
@@ -6,12 +6,6 @@ import (
 	"os"
 )
 
-const (
-	Name     = "tlc3"
-	Version  = "0.0.1"
-	Revision = "HEAD"
-)
-
 func main() {
 	if err := newApp().run(context.Background()); err != nil {
 		fmt.Fprintln(os.Stderr, err)
---
+++ version.go
@@ -0,0 +1,7 @@
+package main
+
+const Name = "tlc3"
+
+const Version = "0.0.1"
+
+var Revision = "HEAD"
