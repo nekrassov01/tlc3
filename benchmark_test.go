8b769327646b874c499d651b92a2e8695eed3e2c
nekrassov01 <unclek62@gmail.com>
2026-01-25 00:33:36 +0900

    chore: update benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -6,9 +6,18 @@ import (
 	"time"
 )
 
-func Benchmark(b *testing.B) {
+func Benchmark_Single(b *testing.B) {
 	for b.Loop() {
 		_, err := GetCerts(context.Background(), []string{"localhost:8443"}, 5*time.Second, true, time.Local)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func Benchmark_Multiple(b *testing.B) {
+	for b.Loop() {
+		_, err := GetCerts(context.Background(), []string{"localhost:8443", "127.0.0.1:8443"}, 5*time.Second, true, time.Local)
 		if err != nil {
 			b.Fatal(err)
 		}
