6eaedf0f5a02c33cae0aca11d18cef754a7fc9b0
nekrassov01 <unclek62@gmail.com>
2026-01-26 14:32:48 +0900

    bump up version to 0.0.24

--- cmd/tlc3/version.go
+++ cmd/tlc3/version.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // Version is the current version of tlc3.
-const version = "0.0.23"
+const version = "0.0.24"
 
 // Revision is the git revision of tlc3.
 var revision = ""
